57ade71e9202108cb5d96152fbb0ca52b50cf800
Bharat <bharat555@users.noreply.github.com>
2017-12-25 18:34:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-
-app = dash.Dash()
 
 
 # This is a test
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    print "hello"
